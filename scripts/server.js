2d5050b07df5f2ca035d8da49d05f73fe4e385b8
Anshul Sirur <anshul@3dhubs.com>
2021-02-09 14:08:54 +0100

    Configurable webhook proxy target

--- scripts/server.js
+++ scripts/server.js
@@ -13,7 +13,7 @@ function proxy() {
   const SmeeClient = require("smee-client");
   const smee = new SmeeClient({
     source: process.env.WEBHOOK_PROXY_URL,
-    target: `http://localhost:3000`
+    target: process.env.WEBHOOK_PROXY_TARGET,
   });
   smee.start();
 }
